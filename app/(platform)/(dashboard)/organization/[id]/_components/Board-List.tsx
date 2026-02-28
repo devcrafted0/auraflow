eb5911e281305b95cfe420f6791089d74404363c
Hassan <pubgabcde2@gmail.com>
2026-02-28 02:06:22 -0800

    Tooltip Done , Plus some simple cards and simple info

---
+++ app/(platform)/(dashboard)/organization/[id]/_components/Board-List.tsx
@@ -0,0 +1,27 @@
+import Hint from "@/components/Hint";
+import { HelpCircle, User2 } from "lucide-react";
+
+const BoardList = () => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center font-semibold text-lg text-neutral-700">
+        <User2 className="h-6 w-6 mr-2" />
+        Your boards
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="aspect-video relative h-full w-full bg-muted rounded-b-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
+          <p className="text-sm">Create new board</p>
+          <span className="text-xs">5 remaining</span>
+          <Hint
+            sideOffset={40}
+            description={`Free Workspaces can have upto 5 open boards. For unlimited boards upgrade this workspace.`}
+          >
+            <HelpCircle className="absolute bottom-2 right-2 h-3.5 w-3.5" />
+          </Hint>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoardList;
---
+++ components/Hint.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+interface HintProps {
+  children: ReactNode;
+  description: string;
+  side?: "left" | "right" | "top" | "bottom";
+  sideOffset?: number;
+}
+
+const Hint = ({
+  children,
+  description,
+  side = "bottom",
+  sideOffset = 0,
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent
+          side={side}
+          sideOffset={sideOffset}
+          className="text-xs max-w-55 wrap-break-word"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default Hint;

f2201008f721c4cb3b2c8c9507e76f16c6ada0c1
Hassan <pubgabcde2@gmail.com>
2026-03-02 02:22:57 -0800

    Lodash worked

--- app/(platform)/(dashboard)/organization/[id]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[id]/layout.tsx
@@ -1,5 +1,16 @@
 import { ReactNode } from "react";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs/server";
 import OrgControl from "./_components/OrgControl";
+
+export async function generateMetadata() {
+  const { orgSlug } = await auth();
+  const cleanSlug = orgSlug?.replace(/-[0-9]+$/, "");
+
+  return {
+    title: startCase(cleanSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: ReactNode }) => {
   return (
